4b7042ef83482bd34f9733e064d2b009ffb29daf
Ramin Afandiyev <41924783+raminafandi@users.noreply.github.com>
2020-07-17 22:03:56 +0400

    Trying to do smt nice about Explore Screen

--- src/screens/ExploreScreen.js
+++ src/screens/ExploreScreen.js
@@ -1,26 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView, Button } from 'react-native';
 
+import { SearchBar } from 'react-native-elements';
+
+import DeliveryCard from '../components/DeliveryCard';
+import HeaderViewAll from '../components/HeaderViewAll';
+import MediumInfoCard from '../components/MediumInfoCard';
+import InfoCard from '../components/InfoCard';
+
+import Pizzas from '../data/pizzas.json';
+import InfoCardsList from '../components/InfoCardsList';
+
 const ExploreScreen = ({}) => {
+  const [search, updateSearch] = useState('');
+
   //   const pressHandler = () => {
   //     navigation.navigate('Details', {});
   //   };
   return (
-    // <ScrollView>
     <View style={styles.container}>
-      <Text>Explore</Text>
+      <SearchBar
+        placeholder="Search in our menu..."
+        onChangeText={updateSearch}
+        value={search}
+        containerStyle={{ backgroundColor: 'red', borderRadius: 15 }}
+        inputContainerStyle={{ backgroundColor: 'white', borderRadius: 15 }}
+        lightTheme
+      />
+      <HeaderViewAll title="New" secTitle="All new, all different!" />
+      <View>
+        <InfoCardsList data={Pizzas.slice(0, 4)} />
+      </View>
+      <View style={styles.menuContainer}>
+        <View style={styles.infocard1}>
+          <InfoCard data={Pizzas[0]} />
+        </View>
+        <View style={styles.infocard2}>
+          <InfoCard data={Pizzas[1]} />
+        </View>
+      </View>
     </View>
-    // </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    paddingVertical: 30,
     backgroundColor: 'white',
+    // height: '100%',
     flex: 1,
+  },
+  Container: {
+    // flex: 3,
     flexDirection: 'row',
     flexWrap: 'wrap',
-    alignItems: 'flex-start',
+    // fle
+  },
+  infocard1: {
+    alignSelf: 'flex-start',
+  },
+  infocard2: {
+    alignSelf: 'flex-end',
   },
 });
 
