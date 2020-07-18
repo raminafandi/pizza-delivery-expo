98b98446f0507a6c183f9eed78f94d346cd1a2a5
Ramin Afandiyev <41924783+raminafandi@users.noreply.github.com>
2020-07-18 22:45:46 +0400

    React Native as always 1m problems

--- src/components/ItemAddedToCart.js
+++ src/components/ItemAddedToCart.js
@@ -5,36 +5,57 @@ const ItemAddedToCart = ({ data }) => {
   return (
     <View style={styles.container}>
       <Image style={styles.img} source={{ uri: data.imgSmall }} />
-      <Text style={styles.text}>{data.name}</Text>
-      <Text style={styles.price}>Starts from {data.sizes[0].price} manat</Text>
+      <View style={styles.textPart}>
+        <Text style={styles.text}>{data.name}</Text>
+        <Text style={styles.price}>{data.sizes[0].price} manat</Text>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    margin: 15,
-    marginLeft: 20,
-    marginRight: 20,
     width: '100%',
+    // flex: 3,
+    flexDirection: 'row',
+    // shadowColor: '#000',
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 12,
+    // },
+    // shadowOpacity: 0.58,
+    // shadowRadius: 16.0,
+
+    // elevation: 24,
+    // justifyContent: '',
   },
   img: {
     width: 100,
     height: 100,
     borderRadius: 20,
-    marginBottom: 5,
+    alignSelf: 'flex-start',
+    // marginBottom: 5,
+  },
+  textPart: {
+    alignSelf: 'flex-end',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
   },
   text: {
     marginLeft: 5,
     fontWeight: 'bold',
     fontSize: 16,
     flex: 1,
-    textAlign: 'left',
+    // textAlign: 'left',
+    alignSelf: 'center',
     // textAlign: 'center',
   },
   price: {
     marginLeft: 5,
     fontSize: 13,
     color: 'grey',
+    justifyContent: 'flex-start',
+    alignSelf: 'flex-start',
+    textAlign: 'center',
   },
 });
 
