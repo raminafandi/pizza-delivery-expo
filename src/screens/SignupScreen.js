d535f45016c748073010ff0769ba0eccfdf9fedb
Ramin Afandiyev <41924783+raminafandi@users.noreply.github.com>
2020-07-23 22:53:09 +0400

    Sign Up done with design

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
-import { Text, Input, Button } from 'react-native-elements';
-import { StyleSheet, View } from 'react-native';
+import { Input, Button } from 'react-native-elements';
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  Dimensions,
+  Text,
+} from 'react-native';
 
 const SignUpScreen = ({}) => {
   const [name, setName] = useState('');
@@ -9,67 +15,106 @@ const SignUpScreen = ({}) => {
   const [passwordConf, setPasswordConf] = useState('');
 
   return (
-    <View style={styles.container}>
-      <View style={styles.modal}>
-        <Text h3>Sign Up</Text>
-        <Input
-          label="Full Name"
-          value={name}
-          onChangeText={(newName) => {
-            setName(newName);
-          }}
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <Input
-          label="Email"
-          value={email}
-          onChangeText={(newEmail) => {
-            setEmail(newEmail);
-          }}
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <Input
-          secureTextEntry
-          label="Password"
-          value={password}
-          onChangeText={(newpassword) => {
-            setPassword(newpassword);
-          }}
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <Input
-          secureTextEntry
-          label="Confirm Password"
-          value={passwordConf}
-          onChangeText={(newpassword) => {
-            setPasswordConf(newpassword);
-          }}
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <Button
-          titleStyle={{ color: 'white', fontSize: 13 }}
-          buttonStyle={[styles.customizeBtn, { backgroundColor: '#F4B400' }]}
-          title="Sign Up"
-        />
-      </View>
+    <View style={styles.container} behavior="padding">
+      <ImageBackground
+        style={styles.img}
+        imageStyle={{
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+        }}
+        source={{
+          uri:
+            'https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg',
+        }}>
+        <View style={styles.modal}>
+          <Text style={styles.header}>Sign Up</Text>
+          <Input
+            label="Full Name"
+            value={name}
+            onChangeText={(newName) => {
+              setName(newName);
+            }}
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input
+            label="Email"
+            value={email}
+            onChangeText={(newEmail) => {
+              setEmail(newEmail);
+            }}
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input
+            secureTextEntry
+            label="Password"
+            value={password}
+            onChangeText={(newpassword) => {
+              setPassword(newpassword);
+            }}
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input
+            secureTextEntry
+            label="Confirm Password"
+            value={passwordConf}
+            onChangeText={(newpassword) => {
+              setPasswordConf(newpassword);
+            }}
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Button
+            titleStyle={{ color: 'white', fontSize: 13 }}
+            buttonStyle={[styles.customizeBtn, { backgroundColor: '#F4B400' }]}
+            title="Sign Up"
+          />
+          <Text style={{ textAlign: 'center' }}>or</Text>
+          <Button
+            titleStyle={{ color: 'white', fontSize: 13 }}
+            buttonStyle={[styles.customizeBtn, { backgroundColor: '#4267B2' }]}
+            title="Sign Up with Facebook"
+          />
+          <Button
+            titleStyle={{ color: 'white', fontSize: 13 }}
+            buttonStyle={[styles.customizeBtn, { backgroundColor: '#DB4437' }]}
+            title="Sign Up with Google"
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 30,
     backgroundColor: 'white',
     flex: 1,
   },
+  img: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  header: {
+    // textAlign: 'center',
+    color: 'red',
+    marginLeft: 8,
+    fontSize: 25,
+    marginBottom: 10,
+  },
   modal: {
+    backgroundColor: 'white',
     margin: 40,
-    borderColor: 'red',
+    padding: 20,
+    borderColor: 'white',
     borderWidth: 1,
+    borderRadius: 28,
+  },
+  customizeBtn: {
+    borderRadius: 10,
+    marginVertical: 5,
   },
 });
 
